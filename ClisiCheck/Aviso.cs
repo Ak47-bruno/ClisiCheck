670d739e3405895ef8b58808731232927bd4a36b
bruno bentes <brunobentes63@hotmail.com>
2022-04-17 19:51:30 -0400

    primeiro commit

---
+++ ClisiCheck/Aviso.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using Microsoft.Win32;
+using System.Diagnostics;
+namespace ClisiCheck
+{
+    class Aviso {
+
+        public void alert(string program)
+        {
+            string message = "Deseja instalar "+ program+"?";
+            string title = "Aviso!";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                string command = "/C notepad.exe";
+                Process.Start("cmd.exe", command);
+            }
+            else
+            {
+                
+            }
+        }
+
+    }
+
+    
+    
+}
+

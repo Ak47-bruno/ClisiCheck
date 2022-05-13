6819844b523daa23bad42e8828b8d52cffcf3024
Bruno The Creator <brunobentes63@gmail.com>
2022-05-12 22:45:45 -0400

    Enter no login.

--- ClisiCheck/Login.Designer.cs
+++ ClisiCheck/Login.Designer.cs
@@ -107,6 +107,7 @@ namespace ClisiCheck
             // 
             // Login
             // 
+            this.AcceptButton = this.btnConfirmar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
